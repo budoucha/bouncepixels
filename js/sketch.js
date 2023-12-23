773ddc8c1bd62351e31e79e3a7e4f68bba20bf6c
budoucha <budoucha@gmail.com>
2023-12-23 14:26:46 +0900

    不意の改行を防止

--- js/sketch.js
+++ js/sketch.js
@@ -64,15 +64,15 @@ const p = new p5(
 
             const scaleElement = document.getElementById("scale")
             const scaleLabelElement = document.getElementById("scaleLabel")
-            scaleLabelElement.innerText = `ballSize: ${scaleElement.value}`
+            scaleLabelElement.innerText = `ballSize: \n${scaleElement.value}`
 
             const ballSetNumElement = document.getElementById("ballSetNum")
             const ballSetNumLabelElement = document.getElementById("ballSetNumLabel")
-            ballSetNumLabelElement.innerText = `ballSetNum: ${ballSetNumElement.value}`
+            ballSetNumLabelElement.innerText = `ballSetNum: \n${ballSetNumElement.value}`
 
             const speedElement = document.getElementById("speed")
             const speedLabelElement = document.getElementById("speedLabel")
-            speedLabelElement.innerText = `speed: ${speedElement.value}`
+            speedLabelElement.innerText = `speed: \n${speedElement.value}`
         }
 
         p.draw = () => {
